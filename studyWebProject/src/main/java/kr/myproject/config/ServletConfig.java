16fa2ae4774e80b371dfb57bd392384fb2bf0536
hakGuenLee <alsfkr23@naver.com>
2023-08-22 14:08:13 +0900

    프로젝트 초기 셋팅

---
+++ studyWebProject/src/main/java/kr/myproject/config/ServletConfig.java
@@ -0,0 +1,34 @@
+package kr.myproject.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages= {"kr.myproject.study","kr.myproject.controller"})
+public class ServletConfig implements WebMvcConfigurer {
+
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {		
+		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+	}
+	
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		registry.viewResolver(bean);
+	}
+	
+	
+	
+	
+}
---
+++ studyWebProject/src/main/java/kr/myproject/config/WebConfig.java
@@ -0,0 +1,38 @@
+package kr.myproject.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+
+		return new Class[] {RootConfig.class};
+	}
+	
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+
+		return new Class[] {ServletConfig.class};
+	}
+	
+	@Override
+	protected String[] getServletMappings() {
+
+		return new String[] {"/"};
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter chEncoder = new CharacterEncodingFilter();
+		chEncoder.setEncoding("UTF-8");
+		chEncoder.setForceEncoding(true);
+		
+		return new Filter[] {chEncoder};
+	}
+	
+	
+}
