1264e4f39993b1de988971b1ead84107d88c0b92
hakGuenLee <alsfkr23@naver.com>
2023-08-22 22:23:49 +0900

    회원가입 컨트롤러 주석 추가

--- studyWebProject/src/main/java/kr/myproject/controller/usermenu/UserRegisterController.java
+++ studyWebProject/src/main/java/kr/myproject/controller/usermenu/UserRegisterController.java
@@ -33,6 +33,7 @@ public class UserRegisterController {
 	}
 	
 	//아이디 중복 체크
+	//문자열 리턴하기 전 UTF-8 인코딩 처리
 	@PostMapping(value = "/idCheck", produces = "application/text; charset=UTF-8")
 	@ResponseBody
 	public String idCheck(@RequestParam("id") String id) {
@@ -49,8 +50,7 @@ public class UserRegisterController {
 		}
 		String rejectMsg = "중복된 아이디입니다!";
 		
-		return rejectMsg; 
-		
+		return rejectMsg; 		
 	}
 	
 }
