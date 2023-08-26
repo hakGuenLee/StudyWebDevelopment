c8adf6deb12308ac6dfd35afe2fe4dcbb5d68158
hakGuenLee <alsfkr23@naver.com>
2023-08-26 10:16:36 +0900

    모임 만들기 처리 일부 변경(테이블 컬럼 변경 및 mapper, dto 변경)

--- studyWebProject/src/main/java/kr/myproject/domain/GroupDTO.java
+++ studyWebProject/src/main/java/kr/myproject/domain/GroupDTO.java
@@ -12,7 +12,7 @@ public class GroupDTO {
 	private String group_key; //모임 고유번호
 	private String group_category; //모임 카테고리
 	private String group_name; //모임명
-	private String gorup_master; //모임 대표자 아이디
+	private String gorup_maker; //모임 개설자 아이디
 	private int group_member; //모임 인원
 	private String group_info; //모임 소개 내용
 	private String use_yn; //모임 활성화 여부
@@ -20,6 +20,7 @@ public class GroupDTO {
 	
 	private String member_id; //참가인원 아이디
 	private String member_nickname; //참가인원 닉네임
-	private String member_name; //참가인원 성명
+	private String member_role; //멤버 권한
+	private Date join_dt; //가입일자
 
 }
